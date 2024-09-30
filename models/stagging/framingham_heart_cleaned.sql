32d3dd8ef058922cb4bff79f39081a5add01158c
valadecharlotte@gmail.com <valadecharlotte@gmail.com>
2024-09-30 09:12:38 +0000

    Initial set up

---
+++ models/stagging/framingham_heart_cleaned.sql
@@ -0,0 +1,8 @@
+-- Mettre à jour CURSMOKE à 0 lorsque CIGPDAY est à 0
+-- UPDATE
+-- `data-analysis-bootcamp-429514.Cardiovascular_recommendations.framingham_heart_study_enriched_period_2`
+-- SET CURSMOKE = 0
+-- WHERE CIGPDAY = 0
+-- AND CURSMOKE = 1;
+select cigpday, case when cigpday = 0 then 0 else cursmoke end as cursmoke
+from `healthcare-hub-436915.raw_data.framingham_heart_study`
